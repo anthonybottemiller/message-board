aac4a8ebb0ea4fa4bc1821d9fe338794c160e9c2
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-13 15:38:58 -0800

    add answer model ith relationship to questions

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   questionText : DS.attr(),
   userName : DS.attr(),
-  notes : DS.attr()
+  notes : DS.attr(),
+  answers: DS.hasMany('answer', { async: true})
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(parameters) {
     return this.store.findRecord('question', parameters.question_id);
+  },
+  actions: {
+    saveAnswer(parameters) {
+      var newAnswer = this.store.createRecord('answer', parameters);
+      var question = parameters.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    }
   }
 });
